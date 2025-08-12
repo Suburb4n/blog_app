3a9f6f01bf805a44495aa91e12d9414a86cf522c
dluis <dluis@cocus.com>
2025-08-12 16:24:05 +0100

    add inputs to make konva components reactive

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
 	import { Stage, Layer, Rect } from '$lib/components/konva';
+	let x = $state(180);
+	let y = $state(90);
+	let fill = $state('#A020F0');
 </script>
 
 <Stage width={500} height={500}>
@@ -7,22 +10,29 @@
 		<Rect
 			ondragend={(e) => console.log(e)}
 			draggable
-			stroke="yellow"
-			strokeWidth={4}
+			stroke="green"
+			strokeWidth={3}
 			width={200}
 			height={200}
 			x={20}
 			y={40}
-			fill="purple"
+			fill="blanchedalmond"
 		/>
+		<Rect width={100} height={100} {x} {y} {fill} stroke="coral" strokeWidth={3}></Rect>
 	</Layer>
 </Stage>
+
+<input type="range" bind:value={x} min={0} max={300} />
+<input type="range" bind:value={y} min={0} max={300} />
+<input type="color" bind:value={fill} />
 
 <style>
 	:global {
 		body {
 			background-color: #222;
 			color: #fff;
+			padding: 0;
+			margin: 0;
 		}
 	}
 </style>
